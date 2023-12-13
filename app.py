70f0979f740993f736befd4ac301edf633166f5d
Mih1rGupt4 <mihirgupta422@gmail.com>
2023-12-13 12:24:57 +0530

    update

--- app.py
+++ app.py
@@ -11,12 +11,6 @@ if 'POSTGRES_PASSWORD_FILE' in os.environ:
 else:
    password = os.environ['POSTGRES_PASSWORD']
 
-# Sample initial data
-products = [
-    {"id": 1, "name": "Product 1", "price": 19.99},
-    {"id": 2, "name": "Product 2", "price": 29.99},
-    {"id": 3, "name": "Product 3", "price": 39.99},
-]
 
 class Product:
     def __init__(self, id, name, price):
